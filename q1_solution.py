f4cca15e3130f759a8faa7712238f04bfc598dff
samer <khsamer11@gmail.com>
2022-05-26 02:15:26 +0300

    q1 updated

--- q1_solution.py
+++ q1_solution.py
@@ -4,15 +4,6 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-# find fundamental matrix
-def fundamentalMatrix(pts1, pts2, key):
-    if key == 8:
-        F, mask = cv2.findFundamentalMat(pts1, pts2, cv2.FM_8POINT)
-    else:
-        F, mask = cv2.findFundamentalMat(pts1, pts2, cv2.FM_7POINT)
-    return F, mask
-
-
 # get matching image points
 def getImagePts(im1, im2, varName1, varName2, nPoints):
     plt.figure()
@@ -38,85 +29,42 @@ def getImagePts(im1, im2, varName1, varName2, nPoints):
         curr = imagePts2[i][0]
         imagePts2[i][0] = imagePts2[i][1]
         imagePts2[i][1] = curr
-    np.save(varName1 + ".npy", imagePts1.round())
-    np.save(varName2 + ".npy", imagePts2.round())
+    np.save("Q1/" + varName1 + ".npy", imagePts1.round())
+    np.save("Q1/" + varName2 + ".npy", imagePts2.round())
 
 
-def computeEpilines(points, whichImage, F, lines=None):
-    if whichImage == 1:
-        lines = np.matmul(F, points)
-        lines = lines.T
-    if whichImage == 2:
-        F = F.T
-        lines = np.matmul(F, points)
-        lines = lines.T
-    return lines
-
-
-def convert2Dto3D(points1, points2):
+def convert2Dto3D(pts1_1, pts1_2, pts2_1, pts2_2):
     homo_pts1 = []
     homo_pts2 = []
-    for point1, point2 in zip(points1, points2):
-        x = point1[0]
-        y = point1[1]
-        z = 1
-        homo_pts1.append((x, y, z))
-        x = point2[0]
-        y = point2[1]
-        z = 1
-        homo_pts2.append((x, y, z))
+    homo_pts3 = []
+    homo_pts4 = []
+    for p1_1, p1_2, p2_1, p2_2 in zip(pts1_1, pts1_2, pts2_1, pts2_2):
+        homo_pts1.append((p1_1[0], p1_1[1], 1))
+        homo_pts2.append((p1_2[0], p1_2[1], 1))
+        homo_pts3.append((p2_1[0], p2_1[1], 1))
+        homo_pts4.append((p2_2[0], p2_2[1], 1))
     homo_pts1 = np.int32(homo_pts1)
     homo_pts2 = np.int32(homo_pts2)
-    return homo_pts1, homo_pts2
-
-
-def SED(pts1, pts2, F):
-    diff = []
-    for i in range(10):
-        xtag = pts2[i].reshape(3, 1)
-        xtag = np.transpose(xtag)
-        x = pts1[i].reshape(3, 1)
-        L = np.matmul(F, x)
-        XF = np.matmul(xtag, F)
-        XFx = np.matmul(XF, x)
-        d = XFx / (math.sqrt(pow(L[0], 2) + pow(L[1], 2)))
-        d = abs(d)
-        diff = np.append(diff, d)
-    avg = np.average(diff)
-    return diff, avg
+    homo_pts3 = np.int32(homo_pts3)
+    homo_pts4 = np.int32(homo_pts4)
+    return homo_pts1, homo_pts2, homo_pts3, homo_pts4
 
 
 def EpipolarDistance(F, points1, points2):
-    # diff = []
-    # for i in range(10):
-    #     xtag = points2[i].reshape(3, 1)
-    #     xtag = np.transpose(xtag)
-    #     x = points1[i].reshape(3, 1)
-    #     L = np.matmul(F, x)
-    #     XF = np.matmul(xtag, F)
-    #     XFx = np.matmul(XF, x)
-    #     d = XFx / (math.sqrt(pow(L[0], 2) + pow(L[1], 2)))
-    #     d = abs(d)
-    #     diff = np.append(diff, d)
-    # avg = np.average(diff)
-    # return diff, avg
-    #
-
-
-    number_of_points = np.size(points1)/3
-    sum = 0
+    number_of_points = np.size(points1) / 3
+    Sum = 0
     for point1, point2 in zip(points1, points2):
         l2 = np.matmul(F, point1.T)
         l2 = l2.T
         divisor = math.sqrt(np.square(l2[0]) + np.square(l2[1]))
-        d1 = np.square(np.matmul(point2, l2.T) / divisor)
+        d1 = np.matmul(point2, l2.T / divisor)
         l1 = np.matmul(F.T, point2.T)
         l1 = l1.T
         divisor = math.sqrt(np.square(l1[0]) + np.square(l1[1]))
-        d2 = np.square(np.matmul(point1, l1.T) / divisor)
-        sum += d1 + d2
-    sum = sum / number_of_points
-    return sum
+        d2 = np.matmul(point1, l1.T / divisor)
+        Sum += d1 + d2
+    Sum = abs(Sum) / number_of_points
+    return Sum
 
 
 def drawLines(img1, img2, lines1, lines2, pts1, pts2):
@@ -135,52 +83,78 @@ def drawLines(img1, img2, lines1, lines2, pts1, pts2):
     return img1, img2
 
 
-def runScript(pts1, pts2, img1, img2):
-    F, mask = fundamentalMatrix(pts1, pts2, 7)
-    homo_pts1, homo_pts2 = convert2Dto3D(pts1, pts2)
-    lines1 = computeEpilines(homo_pts2.T, 2, F)
-    lines2 = computeEpilines(homo_pts1.T, 1, F)
-    img3, img4 = drawLines(img1, img2, lines1, lines2, pts1, pts2)
-    # epi = EpipolarDistance(F, homo_pts1, homo_pts2)
-    diff, epi = SED(homo_pts1, homo_pts2, F)
+def compute(p1, p2, homo_p1, homo_p2, img1, img2, F):
+    left = cv2.computeCorrespondEpilines(p2.reshape(-1, 1, 2), 2, F)
+    left = left.reshape(-1, 3)
 
+    right = cv2.computeCorrespondEpilines(p1.reshape(-1, 1, 2), 1, F)
+    right = right.reshape(-1, 3)
+    img3, img4 = drawLines(img1, img2, left, right, p1, p2)
+
+    epi = EpipolarDistance(F, homo_p1, homo_p2)
+
+    plt.figure()
     plt.subplot(121), plt.imshow(img3)
-    plt.title("8 Point algorithm")
-    plt.subplot(122), plt.imshow(img4)
     plt.title("SED:" + str(round(epi, 4)))
+    plt.subplot(122), plt.imshow(img4)
 
     plt.show()
 
 
+def preparation(img1, img2, location):
+    # getImagePts(img1, img2, location + "s1_1", location + "s1_2", 10)
+    # getImagePts(img1, img2, location + "s2_1", location + "s2_2", 10)
+
+    pts1_1 = np.load("Q1/" + location + "s1_1.npy")
+    pts1_2 = np.load("Q1/" + location + "s1_2.npy")
+    pts2_1 = np.load("Q1/" + location + "s2_1.npy")
+    pts2_2 = np.load("Q1/" + location + "s2_2.npy")
+
+    pts1_1 = [[pt[0], pt[1]] for pt in pts1_1]
+    pts1_2 = [[pt[0], pt[1]] for pt in pts1_2]
+    pts1_1 = np.int32(pts1_1)
+    pts1_2 = np.int32(pts1_2)
+
+    pts2_1 = [[pt[0], pt[1]] for pt in pts2_1]
+    pts2_2 = [[pt[0], pt[1]] for pt in pts2_2]
+    pts2_1 = np.int32(pts2_1)
+    pts2_2 = np.int32(pts2_2)
+    # computing fundamental matrix for s1
+    F, mask = cv2.findFundamentalMat(pts1_1, pts1_2, cv2.FM_8POINT)
+    # for s1 and s2 making 3d points
+    homo_pts1_1, homo_pts1_2, homo_pts2_1, homo_pts2_2 = convert2Dto3D(pts1_1, pts1_2, pts2_1, pts2_2)
+    return pts1_1, pts1_2, pts2_1, pts2_2, homo_pts1_1, homo_pts1_2, homo_pts2_1, homo_pts2_2, F
+
+
 if __name__ == "__main__":
     path1 = "Q1/location_1_frame_001.jpg"
-    img1 = cv2.cvtColor(cv2.imread(path1), cv2.COLOR_BGR2RGB)
+    image1 = cv2.cvtColor(cv2.imread(path1), cv2.COLOR_BGR2RGB)
     path2 = "Q1/location_1_frame_002.jpg"
-    img2 = cv2.cvtColor(cv2.imread(path2), cv2.COLOR_BGR2RGB)
-
-    # getImagePts(img1, img2, "loc1_001", "loc1_002", 10)
-    pts1 = np.load("loc1_001.npy")
-    pts2 = np.load("loc1_002.npy")
-    pts1 = [[pt[0], pt[1]] for pt in pts1]
-    pts2 = [[pt[0], pt[1]] for pt in pts2]
-    pts1 = np.int32(pts1)
-    pts2 = np.int32(pts2)
-
-    runScript(pts1, pts2, img1, img2)
+    image2 = cv2.cvtColor(cv2.imread(path2), cv2.COLOR_BGR2RGB)
 
     path3 = "Q1/location_2_frame_001.jpg"
-    img3 = cv2.cvtColor(cv2.imread(path3), cv2.COLOR_BGR2RGB)
+    image3 = cv2.cvtColor(cv2.imread(path3), cv2.COLOR_BGR2RGB)
     path4 = "Q1/location_2_frame_002.jpg"
-    img4 = cv2.cvtColor(cv2.imread(path4), cv2.COLOR_BGR2RGB)
+    image4 = cv2.cvtColor(cv2.imread(path4), cv2.COLOR_BGR2RGB)
 
-    # getImagePts(img3, img4, "loc2_001", "loc2_002", 10)
-    pts3 = np.load("loc2_001.npy")
-    pts4 = np.load("loc2_002.npy")
-    pts3 = [[pt[0], pt[1]] for pt in pts3]
-    pts4 = [[pt[0], pt[1]] for pt in pts4]
-    pts3 = np.int32(pts3)
-    pts4 = np.int32(pts4)
+    '''
+    location 1
+    '''
+    pts1_1, pts1_2, pts2_1, pts2_2, homo_pts1_1, homo_pts1_2, homo_pts2_1, homo_pts2_2, F = preparation(image1, image2,
+                                                                                                        "location1_")
+    # s1
+    one, two = image1.copy(), image2.copy()
+    compute(pts1_1, pts1_2, homo_pts1_1, homo_pts1_2, one, two, F)
+    # s2
+    compute(pts2_1, pts2_2, homo_pts2_1, homo_pts2_2, image1, image2, F)
 
-    runScript(pts3, pts4, img3, img4)
-
-
+    '''
+    location 2
+    '''
+    pts1_1, pts1_2, pts2_1, pts2_2, homo_pts1_1, homo_pts1_2, homo_pts2_1, homo_pts2_2, F = preparation(image3, image4,
+    "location2_")
+    # s1
+    three, four = image3.copy(), image4.copy()
+    compute(pts1_1, pts1_2, homo_pts1_1, homo_pts1_2, three, four, F)
+    # s2
+    compute(pts2_1, pts2_2, homo_pts2_1, homo_pts2_2, image3, image4, F)
